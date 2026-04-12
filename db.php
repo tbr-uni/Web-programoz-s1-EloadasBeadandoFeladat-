30c100d195a10b674df6246ee1f375a3cbd03baf
galnorbert89 <galnorbe89@gmail.com>
2026-04-12 19:31:00 +0200

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,4 @@
 <?php
-// Adatbázis csatlakozási adatok a Nethely-hez
 $host = "localhost"; 
 
 $db   = "nemesestoth"; 
@@ -9,15 +8,11 @@ $user = "nemesestoth";
 $pass = "3Webprog3"; 
 
 try {
-    // PDO kapcsolat létrehozása az órai beállításokkal
-    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=UTF8", $user, $pass, [
-        // Hibakezelés bekapcsolása (Exception dobása hiba esetén)
+        $pdo = new PDO("mysql:host=$host;dbname=$db;charset=UTF8", $user, $pass, [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        // Alapértelmezett lekérdezési mód: asszociatív tömb
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
     ]);
 } catch (PDOException $e) {
-    // Ha a kapcsolódás sikertelen, leállítjuk a futást
-    die("Database connection failed: " . $e->getMessage());
+        die("Database connection failed: " . $e->getMessage());
 }
 ?>
